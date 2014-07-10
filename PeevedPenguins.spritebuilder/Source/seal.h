ff7d86d198b365eecd73dc0e8ce9c14920d85538
Administrator <Administrator@Lomanstop.local>
2014-07-10 13:20:41 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/seal.h
+++ PeevedPenguins.spritebuilder/Source/seal.h
@@ -1,9 +1,9 @@
 //
 //  seal.h
-//  PeevedPenguins
+//  Chipmunk7-ios
 //
 //  Created by Administrator on 7/10/14.
-//  Copyright (c) 2014 Apportable. All rights reserved.
+//
 //
 
 #import "CCSprite.h"
---
+++ PeevedPenguins.spritebuilder/Source/seal.m
@@ -0,0 +1,13 @@
+//
+//  seal.m
+//  Chipmunk7-ios
+//
+//  Created by Administrator on 7/10/14.
+//
+//
+
+#import "seal.h"
+
+@implementation seal
+
+@end
